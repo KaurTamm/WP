66b2badf2c4a0bcd162f35f93cab2865dec1a2bb
Kaur <kaur.tamm@khk.ee>
2020-03-25 13:02:44 +0200

    modified plugin `My Plugin` version 1.0 (after activation of `My Plugin` version 1.0)

---
+++ wp-content/plugins/myplugin/myplugin.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: My Plugin
+Plugin URI: https://tammkaur.ikt.khk.ee/
+Description: This is my first WordPress Plugin
+Author: Kaur Tamm
+Author URI: https://tammkaur.ikt.khk.ee/
+Version: 1.0
+*/
